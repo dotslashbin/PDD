fa8a5d18d564ecf69129171411e5a1aeae0b20f6
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-29 14:11:48 +0800

    Initial linted setup

--- pdd_webapp/src/App.tsx
+++ pdd_webapp/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+
+// const useStyles = makeStyles(theme => ({
+  
+// })
+
+
+export default function App() {
+
+	const classes = useStyles()
+
+	return (
+		<Container className="App">
+		</Container>
+	)
 }
-
-export default App;
