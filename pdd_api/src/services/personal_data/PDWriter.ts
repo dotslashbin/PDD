91d450fbc059811afa5ec71c4d98f4ecf3ff679a
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-30 17:31:06 +0800

    Implemented call to encrypting data

--- pdd_api/src/services/personal_data/PDWriter.ts
+++ pdd_api/src/services/personal_data/PDWriter.ts
@@ -2,11 +2,31 @@ import { DBWriter } from '../../structures/interfaces'
 import { Payload } from '../../structures/types'
 import { getModelForClass } from '@typegoose/typegoose'
 import { PersonalData } from '../../models/PersonalData'
+import { Encrypt } from '../../helpers/Utilities'
+import crypto from 'crypto'
+
 export default class PDWriter {
 	static async Create(params: Payload, db: DBWriter): Promise<any> {
 		try {
 			const model = getModelForClass(PersonalData)
-			return db.Save(params, model)
+
+			const { email, fullName, attachment } = params
+
+			const iv = crypto.randomBytes(16)
+
+			const encEmail = Encrypt(email, iv)
+			const encFullName = Encrypt(fullName, iv)
+
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			return db.Save(
+				{
+					email: encEmail.content,
+					fullName: encFullName.content,
+					attachment,
+					iv: encEmail.iv,
+				},
+				model
+			)
 		} catch (error) {
 			return error
 		}
