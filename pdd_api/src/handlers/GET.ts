37bea2c2054e247a74d147cdfa3af8f48d3cbb8d
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-01 14:47:19 +0800

    Proper implementation of handler

--- pdd_api/src/handlers/GET.ts
+++ pdd_api/src/handlers/GET.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express'
+import { MongoReader } from '../db/Mongodb/MongoReader'
+import PDReader from '../services/personal_data/PDReader'
 export async function Fetch(
 	request: Request,
 	response: Response
 ): Promise<void> {
-	const { token } = request.query
-	console.log(token)
-
+	const { token, secretKey } = request.query
+	const db = new MongoReader()
+	await PDReader.Fetch({ token, secretKey }, db)
 	response.status(200)
 	response.json(token)
 }
