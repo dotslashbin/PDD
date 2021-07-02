2f6419a41c027357cf586c265bde9256f5896b67
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-02 11:49:53 +0800

    Implemented checking from the blacklist

--- pdd_api/src/middlewares/Authentication.ts
+++ pdd_api/src/middlewares/Authentication.ts
@@ -3,6 +3,8 @@ import TokenBlackListWriter from '../services/auth/TokenBlacklistWriter'
 import { ReturnError } from '../helpers/Response'
 import TokenValidator from '../services/auth/TokenValidator'
 import MongoWriter from '../db/Mongodb/MongoWriter'
+import { MongoReader } from '../db/Mongodb/MongoReader'
+import TokenBlacklistReader from '../services/auth/TokenBlacklistReader'
 
 /**
  * Authentication middleware.
@@ -14,7 +16,7 @@ import MongoWriter from '../db/Mongodb/MongoWriter'
  * @param next
  */
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const AuthenticateToken = (
+export const AuthenticateToken = async (
 	request: Request,
 	response: Response,
 	next: NextFunction
@@ -24,8 +26,17 @@ export const AuthenticateToken = (
 		token = token + ''
 		secretKey = secretKey + ''
 
-		const tokenValidationResult = TokenValidator.Decode(token, secretKey)
+		if ((await isTokenBlacklsited(token)) === true) {
+			ReturnError(
+				401,
+				response,
+				'blacklisted-token',
+				'Your token is no longer active.'
+			)
+			return
+		}
 
+		const tokenValidationResult = TokenValidator.Decode(token, secretKey)
 		if (tokenValidationResult.type !== 'valid') {
 			ReturnError(
 				401,
@@ -59,7 +70,21 @@ export const AuthenticateToken = (
 	next()
 }
 
+/**
+ *
+ * @param token Saves a token into the blacklist
+ */
 const dumpTokenToBlacklist = (token: string): void => {
 	const db = new MongoWriter()
 	TokenBlackListWriter.AddToList(token, db)
+}
+
+/**
+ *
+ * @param token Checks to see if token is blacklisted
+ * @returns
+ */
+const isTokenBlacklsited = (token: string): boolean => {
+	const db = new MongoReader()
+	return TokenBlacklistReader.IsBlacklistedToken(token, db)
 }
