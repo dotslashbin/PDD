dc3c47499814c42d40f2b1ecd45d5e53c7f02172
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-02 23:13:09 +0800

    Added helper to filter out messages

--- pdd_webapp/src/views/notices/DisplayMessage.tsx
+++ pdd_webapp/src/views/notices/DisplayMessage.tsx
@@ -1,13 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 import { Alert, AlertTitle } from '@material-ui/lab/'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+	messagesContainer: {
+		marginBottom: '5px'
+	}, 
+	message: {
+		fontWeight: 'bold'
+	}
+}))
 
 export default function DisplayMessage(props: any): any {
+
+	const classes = useStyles()
 
 	return (
 		<div>
 			<Alert severity={props.severity}>
 				<AlertTitle>{props.severity.toUpperCase() }</AlertTitle>
-				{props.message}
+				<div className={classes.messagesContainer}>
+					{props.messages.map((message: string) => (
+						// eslint-disable-next-line react/jsx-key
+						<div className={classes.message}>{message}</div>
+					))}
+				</div>
 			</Alert>
 		</div>
 	)
