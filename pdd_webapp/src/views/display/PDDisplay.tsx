566812ec842747f0aedf78f13a2acf1651cccceb
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-03 13:10:43 +0800

    Implemented displaying of errors on view page

--- pdd_webapp/src/views/display/PDDisplay.tsx
+++ pdd_webapp/src/views/display/PDDisplay.tsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import { Container } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Container, makeStyles } from '@material-ui/core'
 import PersonalDataSelector from '../../selectors/PersonalData'
+import { GetMessagesArray } from '../../helpers/Messages'
+import DisplayMessage from '../notices/DisplayMessage'
+
+const useStyles = makeStyles((theme) => ({
+	pageMessage: {
+		marginTop: '50px',
+		marginBottom: '50px',
+	}
+}))
 
 const fetchData = async (token:string, secretKey: string):Promise<any> => {
 	return PersonalDataSelector.Fetch(token, secretKey).then(result => result).catch(error => error)
 }
 
 export default function PDDisplay(props: any): any {
+
+	const classes = useStyles()
+
 	const search = props.location.search
 	const params = new URLSearchParams(search)
-
 	const token = params.get('token')
 	const secretKey = params.get('secretKey')
+
+	const [message, setMessage] = useState('')
+	const [severity, setSeverity] = useState('')
 
 	if(token  && secretKey) {
 		fetchData(token, secretKey).then(result => {
 			console.log(result)
+			if(result.status === 401) { // This means an error
+				setSeverity('error')
+				setMessage(result.data.message)
+			}
 		})
 	}
 
 	return(
 		<Container>
-			Here isthe display
+			<div className={classes.pageMessage}>
+				<DisplayMessage messages={GetMessagesArray(message)} severity={severity} />
+			</div>
 		</Container>
 	)
 }
