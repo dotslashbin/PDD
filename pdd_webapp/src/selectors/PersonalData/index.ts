0df77f0310c97c1b6567707dab9233be4e8e8b7e
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-03 12:57:39 +0800

    WIP: initial fetching

--- pdd_webapp/src/selectors/PersonalData/index.ts
+++ pdd_webapp/src/selectors/PersonalData/index.ts
@@ -1,6 +1,17 @@
 import axios from 'axios'
 
+/**
+ * Static class that is responsible for making axios calls to the API
+ */
 export default class PersonalDataSelector {
+	/**
+	 * Calls API to create a new record
+	 * @param email
+	 * @param fullName
+	 * @param expiry
+	 * @param attachment
+	 * @returns
+	 */
 	static async Create(
 		email: string,
 		fullName: string,
@@ -17,9 +28,25 @@ export default class PersonalDataSelector {
 			.then((result) => {
 				return result.data
 			})
-			.catch((error) => {
-				const { status, message } = error.response.data
-				return { error:true, status, message }
+			.catch((createError) => {
+				const { status, message } = createError.response.data
+				return { error: true, status, message }
+			})
+	}
+	/**
+	 * Calls API to fetch a record with the token and secret
+	 * @param token
+	 * @param secretKey
+	 * @returns
+	 */
+	static async Fetch(token: string, secretKey: string): Promise<any> {
+		return axios
+			.get(`http://localhost:3001/personal-data?token=${token}&secretKey=${secretKey}`)
+			.then((result) => {
+				return result.data
+			})
+			.catch((fetchError) => {
+				return fetchError.response
 			})
 	}
 }
--- pdd_webapp/src/views/display/PDDisplay.tsx
+++ pdd_webapp/src/views/display/PDDisplay.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
+import PersonalDataSelector from '../../selectors/PersonalData'
 
-export default function PDDisplay(): any {
+const fetchData = async (token:string, secretKey: string):Promise<any> => {
+	return PersonalDataSelector.Fetch(token, secretKey).then(result => result).catch(error => error)
+}
+
+export default function PDDisplay(props: any): any {
+	const search = props.location.search
+	const params = new URLSearchParams(search)
+
+	const token = params.get('token')
+	const secretKey = params.get('secretKey')
+
+	if(token  && secretKey) {
+		fetchData(token, secretKey).then(result => {
+			console.log(result)
+		})
+	}
+
 	return(
 		<Container>
 			Here isthe display
