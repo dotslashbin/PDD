c360201850f36366d98e0ae783a969c8f6b5ab04
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-03 14:59:13 +0800

    Implemented populating of personal data

--- pdd_webapp/src/views/display/PDDisplay.tsx
+++ pdd_webapp/src/views/display/PDDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, makeStyles } from '@material-ui/core'
 import PersonalDataSelector from '../../selectors/PersonalData'
 import { GetMessagesArray } from '../../helpers/Messages'
@@ -15,32 +15,45 @@ const fetchData = async (token:string, secretKey: string):Promise<any> => {
 	return PersonalDataSelector.Fetch(token, secretKey).then(result => result).catch(error => error)
 }
 
+const personalDataSkeleton = { email:'', fullName: '', attachment: '' }
+
 export default function PDDisplay(props: any): any {
 
 	const classes = useStyles()
 
+	const [message, setMessage] = useState('')
+	const [severity, setSeverity] = useState('')
+	const [email, setEmail] = useState('')
+	const [personalData, setPersonalData] = useState(personalDataSkeleton)
+
+
 	const search = props.location.search
+
 	const params = new URLSearchParams(search)
 	const token = params.get('token')
 	const secretKey = params.get('secretKey')
 
-	const [message, setMessage] = useState('')
-	const [severity, setSeverity] = useState('')
-
-	if(token  && secretKey) {
-		fetchData(token, secretKey).then(result => {
-			console.log(result)
-			if(result.status === 401) { // This means an error
-				setSeverity('error')
-				setMessage(result.data.message)
-			}
-		})
-	}
+	useEffect(() => {
+		if(token && secretKey) {
+			fetchData(token, secretKey).then(result => {
+				if(result.status === 401) {
+					setSeverity('error')
+					setMessage(result.data.message)
+					setPersonalData(personalDataSkeleton)
+				} else if (result.status === 200) {
+					setPersonalData(result.data)
+				}
+			})
+		}
+	}, [])
 
 	return(
 		<Container>
 			<div className={classes.pageMessage}>
 				<DisplayMessage messages={GetMessagesArray(message)} severity={severity} />
+			</div>
+			<div>
+				{(personalData && personalData.email)? personalData.email:''}
 			</div>
 		</Container>
 	)
