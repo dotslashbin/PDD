a8fc30d980caeb1f5b63581b6c322df8b58f0d73
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-03 15:48:27 +0800

    Implemented displaying of fields

--- pdd_webapp/src/views/display/PDDisplay.tsx
+++ pdd_webapp/src/views/display/PDDisplay.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Container, makeStyles } from '@material-ui/core'
+import { Card, CardContent, Container, makeStyles, Typography } from '@material-ui/core'
 import PersonalDataSelector from '../../selectors/PersonalData'
 import { GetMessagesArray } from '../../helpers/Messages'
 import DisplayMessage from '../notices/DisplayMessage'
@@ -8,7 +8,24 @@ const useStyles = makeStyles((theme) => ({
 	pageMessage: {
 		marginTop: '50px',
 		marginBottom: '50px',
-	}
+	},
+	root: {
+		minWidth: 275,
+	},
+	email: {
+		color: 'red'
+	},
+	bullet: {
+		display: 'inline-block',
+		margin: '0 2px',
+		transform: 'scale(0.8)',
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
 }))
 
 const fetchData = async (token:string, secretKey: string):Promise<any> => {
@@ -23,7 +40,7 @@ export default function PDDisplay(props: any): any {
 
 	const [message, setMessage] = useState('')
 	const [severity, setSeverity] = useState('')
-	const [email, setEmail] = useState('')
+	const [status, setStatus] = useState(0)
 	const [personalData, setPersonalData] = useState(personalDataSkeleton)
 
 
@@ -37,10 +54,12 @@ export default function PDDisplay(props: any): any {
 		if(token && secretKey) {
 			fetchData(token, secretKey).then(result => {
 				if(result.status === 401) {
+					setStatus(result.status)
 					setSeverity('error')
 					setMessage(result.data.message)
 					setPersonalData(personalDataSkeleton)
 				} else if (result.status === 200) {
+					setStatus(result.status)
 					setPersonalData(result.data)
 				}
 			})
@@ -49,12 +68,35 @@ export default function PDDisplay(props: any): any {
 
 	return(
 		<Container>
-			<div className={classes.pageMessage}>
-				<DisplayMessage messages={GetMessagesArray(message)} severity={severity} />
-			</div>
-			<div>
-				{(personalData && personalData.email)? personalData.email:''}
-			</div>
+			{(status === 401)? 
+				<div className={classes.pageMessage}>
+					<DisplayMessage messages={GetMessagesArray(message)} severity={severity} />
+				</div>: 
+				<div>
+					<Card className={classes.root} variant="outlined">
+						<CardContent>
+							<Typography className={classes.title} color="textSecondary" gutterBottom>
+							You are viewing the personal data  of ... 
+							</Typography>
+							<Typography variant="h5" component="h2">
+								{personalData.fullName}
+							</Typography>
+							<Typography className={classes.title} color="textSecondary" gutterBottom>
+							Try reaching out through: 
+							</Typography>
+							<Typography variant="subtitle1">
+								<span className={classes.email}>{personalData.email}</span>
+							</Typography>
+						</CardContent>
+					</Card>
+					<hr />
+					<Card>
+						<CardContent>
+						
+						</CardContent>
+					</Card>
+				</div>
+			}
 		</Container>
 	)
 }
