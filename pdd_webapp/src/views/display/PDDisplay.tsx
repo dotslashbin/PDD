180d88c545e0e665103dc636b160493f9e53e74b
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-07-03 12:29:06 +0800

    Implemented basic routing

---
+++ pdd_webapp/src/views/display/PDDisplay.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+
+export default function PDDisplay(): any {
+	return(
+		<Container>
+			Here isthe display
+		</Container>
+	)
+}
